7f0971286314a503399450715605c4aec0291dfe
Daliloup <sanchezdali99@gmail.com>
2022-07-18 10:49:00 +0200

    Advance 2
    - More bases

--- game.py
+++ game.py
@@ -2,13 +2,23 @@ import pygame
 from sys import exit
 
 pygame.init()
+
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Game')
 pygame.display.set_icon(pygame.image.load('images/flag.png'))
+
 clock = pygame.time.Clock()
+
+font = pygame.font.Font(None, 50)
 
 surface = pygame.Surface((100, 100))
 surface.fill('blue')
+
+text_surface = font.render('GAME', False, 'white')
+
+screen.blit(surface, (0, 0))
+screen.blit(text_surface, (400 - text_surface.get_width()//2, 50))
+
 
 while True:
     for event in pygame.event.get():
@@ -16,7 +26,6 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(surface, (0, 0))
-
+    surface.scroll(10, 10)
     pygame.display.update()
     clock.tick(60)

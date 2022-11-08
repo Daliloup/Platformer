7b74b5fee12352ecad41b34fd886f236258d645f
Daliloup <sanchezdali99@gmail.com>
2022-11-08 22:41:09 +0100

    tmx to pytmx

--- tmx_reader.py
+++ tmx_reader.py
@@ -1,7 +1,7 @@
-import tmx
+import pytmx
 import pygame
 
-map = tmx.TileMap.load('Levels/Level_1/map.tmx')
+map = pytmx.TiledMap('Levels/Level_1/map.tmx')
 
 
 def get_properties(map):
@@ -13,11 +13,68 @@ def get_properties(map):
 
 def get_tilesets(map):
     for tileset in map.tilesets:
-        
+        pass
 
 
-map_properties = get_properties(map)
-get_tilesets(map)
+class Tileset:
+    def __init__(self, tileset) -> None:
+        tileset_img = pygame.image.load(
+            'Levels/Level_1/' + tileset.source
+        ).convert_alpha()
+        self.images = []
+        self.tilecount = tileset.tilecount
+        self.firstgid = tileset.firstgid
+
+        w = tileset.tilewidth
+        h = tileset.tileheight
+        for i in range(tileset.tilecount - tileset.columns + 1):
+            for j in range(tileset.columns):
+                self.images.append(tileset_img.subsurface(w*j, h*i, w, h))
+
+    def __getitem__(self, id):
+        try:
+            return self.images[id]
+        except KeyError:
+            return
 
 
-print(map_properties)
+def display_tileset(screen, tileset, y):
+    for i in range(tileset.tilecount):
+        screen.blit(tileset[i], (i*64, y))
+
+
+def main() -> None:
+    pygame.init()
+
+    # General
+    screen = pygame.display.set_mode((1024, 512))
+    pygame.display.set_caption('Game')
+
+    clock = pygame.time.Clock()
+    FPS = 60
+    pressed_keys = set()
+
+    while True:
+        # Event management
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+            if event.type == pygame.KEYDOWN:
+                pressed_keys.add(event.key)
+            elif event.type == pygame.KEYUP:
+                pressed_keys.remove(event.key)
+
+        screen.fill((255, 255, 255))
+
+        # for i in range(len(map.tilesets)):
+        #     display_tileset(screen, Tileset(map.tilesets[i]), i*64)
+
+        clock.tick(FPS)
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    print(map.layers)
+    print('\n', map.layers, sep='')
